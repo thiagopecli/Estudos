c1843e760ed49c8864dfa6cc7cfd2089ec161925
Thiago Pereira <148633999+thiagopecli@users.noreply.github.com>
2024-02-11 18:39:07 -0300

    teste variáveis

---
+++ testevariaveis.py
@@ -0,0 +1,21 @@
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Thiago', 28
+estado = True
+
+# Função type()
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
+
+# Função Isinstance()
+a = 10
+b = 'Sol'
+print(isinstance(a, (int, float)))
+
+a = 40
+c = 3
+r = a * c
+print(r)
